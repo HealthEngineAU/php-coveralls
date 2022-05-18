9ad31119cccd68924081635dcb28e9e2f2f7d6e3
Tom Spencer <thomas.spencer@healthengine.com.au>
2022-05-18 18:35:54 +0800

    Add support for Buildkite (#1)

--- src/Bundle/CoverallsBundle/Collector/CiEnvVarsCollector.php
+++ src/Bundle/CoverallsBundle/Collector/CiEnvVarsCollector.php
@@ -59,6 +59,7 @@ class CiEnvVarsCollector
         $this->readEnv = [];
 
         $this
+            ->fillBuildkite()
             ->fillTravisCi()
             ->fillCircleCi()
             ->fillAppVeyor()
@@ -198,6 +199,34 @@ class CiEnvVarsCollector
             $this->readEnv['APPVEYOR_JOB_NUMBER'] = $this->env['APPVEYOR_JOB_NUMBER'];
             $this->readEnv['APPVEYOR_REPO_BRANCH'] = $this->env['APPVEYOR_REPO_BRANCH'];
             $this->readEnv['APPVEYOR_PULL_REQUEST_NUMBER'] = $this->env['APPVEYOR_PULL_REQUEST_NUMBER'];
+            $this->readEnv['CI_NAME'] = $this->env['CI_NAME'];
+        }
+
+        return $this;
+    }
+
+    /**
+     * Fill Buildkite environment variables.
+     *
+     * "BUILDKITE", "BUILDKITE_BUILD_NUMBER" must be set.
+     *
+     * @return $this
+     */
+    protected function fillBuildkite()
+    {
+        if (isset($this->env['BUILDKITE']) && $this->env['BUILDKITE'] && isset($this->env['BUILDKITE_BUILD_NUMBER'])) {
+            $this->env['CI_BRANCH'] = $this->env['BUILDKITE_BRANCH'];
+            $this->env['CI_BUILD_NUMBER'] = $this->env['BUILDKITE_BUILD_NUMBER'];
+            $this->env['CI_JOB_ID'] = $this->env['BUILDKITE_JOB_ID'];
+            $this->env['CI_NAME'] = 'Buildkite';
+            $this->env['CI_PULL_REQUEST'] = $this->env['BUILDKITE_PULL_REQUEST'];
+
+            // backup
+            $this->readEnv['BUILDKITE'] = $this->env['BUILDKITE'];
+            $this->readEnv['BUILDKITE_BRANCH'] = $this->env['BUILDKITE_BRANCH'];
+            $this->readEnv['BUILDKITE_JOB_ID'] = $this->env['BUILDKITE_JOB_ID'];
+            $this->readEnv['BUILDKITE_BUILD_NUMBER'] = $this->env['BUILDKITE_BUILD_NUMBER'];
+            $this->readEnv['BUILDKITE_PULL_REQUEST'] = $this->env['BUILDKITE_PULL_REQUEST'];
             $this->readEnv['CI_NAME'] = $this->env['CI_NAME'];
         }
 
